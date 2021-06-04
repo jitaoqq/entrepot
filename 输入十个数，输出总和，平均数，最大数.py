78e30fae4fb9e70d1cc768ffc4093d2cc419ebd8
jitaoqq <84774386+jitaoqq@users.noreply.github.com>
2021-06-04 14:18:43 +0800

    Add files via upload

---
+++ 输入十个数，输出总和，平均数，最大数.py
@@ -0,0 +1,42 @@
+a = input("请输入第1个数")
+a=int(a)
+b = input("请输入第2个数")
+b=int(b)
+c = input("请输入第3个数")
+c=int(c)
+d = input("请输入第4个数")
+d=int(d)
+e = input("请输入第5个数")
+e=int(e)
+f = input("请输入第6个数")
+f=int(f)
+g = input("请输入第7个数")
+g=int(g)
+h = input("请输入第8个数")
+h=int(h)
+i = input("请输入第9个数")
+i=int(i)
+j = input("请输入第10个数")
+j=int(j)
+p=(a+b+c+d+e+f+g+h+i+j)
+m=(a+b+c+d+e+f+g+h+i+j)/10
+if a>b and a>c and a>d and a>e and a>f and a>g and a>h and a>i and a>j:
+    print("最大数：",a,"总和为：",p,"平均数为：",m,)
+if b>a and b>j and b>c and b>d and b>e and b>f and b>g and b>h and b>i:
+    print("最大数：",b,"总和为：",p,"平均数为：",m,)
+if c>a and c>j and c>b and c>d and c>e and c>f and c>g and c>h and c>i:
+    print("最大数：",c,"总和为：",p,"平均数为：",m,)
+if d>a and d>j and d>c and d>b and d>e and d>f and d>g and d>h and d>i:
+    print("最大数：",d,"总和为：",p,"平均数为：",m,)
+if e>a and e>j and e>c and e>d and e>b and e>f and e>g and e>h and e>i:
+    print("最大数：",e,"总和为：",p,"平均数为：",m,)
+if f>a and f>j and f>c and f>d and f>e and f>b and f>g and f>h and f>i:
+    print("最大数：",f,"总和为：",p,"平均数为：",m,)
+if g>a and g>j and g>c and g>d and g>e and g>f and g>b and g>h and g>i:
+    print("最大数：",g,"总和为：",p,"平均数为：",m,)
+if h>a and h>j and h>c and h>d and h>e and h>f and h>g and h>b and h>i:
+    print("最大数：",h,"总和为：",p,"平均数为：",m,)
+if i>a and i>j and i>c and i>d and i>e and i>f and i>g and i>h and i>b:
+    print("最大数：",i,"总和为：",p,"平均数为：",m,)
+if j>a and j>b and j>c and j>d and j>e and j>f and j>g and j>h and j>i:
+    print("最大数：",j,"总和为：",p,"平均数为：",m,)
